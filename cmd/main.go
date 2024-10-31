36c2f17f4813e91566d6c9874649d98128c8a9d9
Gooseyara <jaromir03@mail.ru>
2024-10-31 19:49:59 +0300

    task-3

--- cmd/main.go
+++ cmd/main.go
@@ -1,53 +1,72 @@
 package main
 
-import (
-	"fmt"
-	"go-core-task/config"
-	"go-core-task/internal/sliceAssembler/UseCase"
-	"log"
-)
+import "fmt"
 
-func main() {
-	log.Println("Starting...")
-	log.Println("Loading config...")
-	v, err := config.LoadConfig()
-	if err != nil {
-		log.Fatalf("error loading config: %v", err)
-	}
-
-	cfg, err := config.ParseConfig(v)
-	if err != nil {
-		log.Fatalf("error parsing config: %v", err)
-	}
-
-	log.Println("Config loaded")
-
-	sliceManipulate(*cfg)
+type StringIntMap struct {
+	data map[string]int
 }
 
-func sliceManipulate(cfg config.Config) {
-	sliceAssemblerUC := UseCase.NewSliceAssembler()
-
-	originalSlice := sliceAssemblerUC.NewRandomSlice(cfg.Slice.Length, cfg.Slice.MinimalValue, cfg.Slice.MaximumValue)
-	fmt.Println("\nOriginal slice: ")
-	fmt.Println(originalSlice)
-
-	evenSlice := sliceAssemblerUC.SliceExample(originalSlice)
-	fmt.Println("\nEven slice: ")
-	fmt.Println(evenSlice)
-
-	sliceWithAdditionalNum := sliceAssemblerUC.AddElements(cfg.Slice.AdditionalNumber, originalSlice)
-	fmt.Println("\nSlice with additional number ")
-	fmt.Println(sliceWithAdditionalNum)
-
-	copiedSlice := sliceAssemblerUC.CopySlice(originalSlice)
-	fmt.Println("\nCopied slice: ")
-	fmt.Println(copiedSlice)
-
-	removedIndexSlice, err := sliceAssemblerUC.RemoveElement(cfg.Slice.IndexForRemove, originalSlice)
-	if err != nil {
-		log.Println(err.Error())
+func NewStringIntMap() *StringIntMap {
+	return &StringIntMap{
+		data: make(map[string]int),
 	}
-	fmt.Println("\nSlice with removed index: ")
-	fmt.Println(removedIndexSlice)
+}
+
+func (m *StringIntMap) Add(key string, value int) {
+	m.data[key] = value
+}
+
+func (m *StringIntMap) Remove(key string) {
+	delete(m.data, key)
+}
+
+func (m *StringIntMap) Copy() map[string]int {
+	newMap := make(map[string]int)
+	for k, v := range m.data {
+		newMap[k] = v
+	}
+	return newMap
+}
+
+func (m *StringIntMap) Exists(key string) bool {
+	_, exists := m.data[key]
+	return exists
+}
+
+func (m *StringIntMap) Get(key string) (int, bool) {
+	value, exists := m.data[key]
+	return value, exists
+}
+
+func main() {
+	m := NewStringIntMap()
+
+	m.Add("ine", 1)
+	m.Add("two", 2)
+	m.Add("three", 3)
+
+	if value, exists := m.Get("1"); exists {
+		fmt.Printf("Key: one, Value: %d\n", value)
+	} else {
+		fmt.Println("Key: one does not exist")
+	}
+
+	if m.Exists("2") {
+		fmt.Println("Key: two exists")
+	} else {
+		fmt.Println("Key: two does not exist")
+	}
+
+	m.Remove("2")
+	if !m.Exists("2") {
+		fmt.Println("Key: two has been removed")
+	}
+
+	copiedMap := m.Copy()
+	fmt.Println("Copied Map:", copiedMap)
+
+	fmt.Println("Original Map:")
+	for key, value := range m.Copy() {
+		fmt.Printf("Key: %s, Value: %d\n", key, value)
+	}
 }
