b6e0d86ed1c07ea7f8675c085a6684c21f9eb3cc
Gooseyara <jaromir03@mail.ru>
2024-11-01 17:32:57 +0300

    task-6

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func FindSliceCoincidences(slice1, slice2 []int) ([]int, bool) {
-	elementCount := make(map[int]int)
-
-	for _, v := range slice1 {
-		elementCount[v] = 1
+func RandomNumberGenerator(ch chan int) {
+	for {
+		num := rand.Intn(100)
+		ch <- num
 	}
-
-	for _, v := range slice2 {
-		_, ok := elementCount[v]
-		if !ok {
-			elementCount[v] = 1
-		} else {
-			elementCount[v] = elementCount[v] + 1
-		}
-	}
-
-	var result []int
-	for key, val := range elementCount {
-		if val == 2 {
-			result = append(result, key)
-		}
-	}
-	if len(result) == 0 {
-		return nil, false
-	}
-	return result, true
 }
 
 func main() {
-	a := []int{65, 3, 58, 678, 64}
-	b := []int{64, 2, 3, 43}
-	fmt.Println(FindSliceCoincidences(a, b))
+	rand.NewSource(time.Now().UnixNano())
+
+	ch := make(chan int)
+
+	go RandomNumberGenerator(ch)
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-ch)
+	}
 }
